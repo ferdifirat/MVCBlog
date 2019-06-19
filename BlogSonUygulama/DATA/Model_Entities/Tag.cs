38e8c9468989090a44a2fafe2cb41553d5cbc262
Ferdi <ferdi269@gmail.com>
2019-06-19 22:38:05 +0300

    commited

---
+++ BlogSonUygulama/DATA/Model_Entities/Tag.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DATA.Model_Entities
+{
+    //Etiket
+    public class Tag
+    {
+        public int TagID { get; set; }
+        public string TagName { get; set; }
+
+        public virtual List<ArticleTag> ArticleTags { get; set; }
+
+    }
+}
