7d4c97ffd51da5169115daed16238d97c4085b0f
Ferdi <ferdi269@gmail.com>
2019-06-20 23:09:56 +0300

    designpatterns added

---
+++ BlogProjesi/DAL/Repository/Concrete/ApplicationUserConcrete.cs
@@ -0,0 +1,25 @@
+﻿using DAL.Repository.Abstract;
+using DATA.Model_Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repository.Concrete
+{
+   public class ApplicationUserConcrete
+    {
+
+        public IRepository<ApplicationUser> _applicationUserRepository;
+        public IUnitOfWork _applicationUserUnitOfWork;
+        private Context _dbContext;
+
+        public ApplicationUserConcrete()
+        {
+            _dbContext = new Context();
+            _applicationUserUnitOfWork = new EFUnitOfWork(_dbContext);
+            _applicationUserRepository = _applicationUserUnitOfWork.GetRepository<ApplicationUser>();
+        }
+    }
+}
---
+++ BlogProjesi/DAL/Repository/Concrete/ArticleConcrete.cs
@@ -0,0 +1,24 @@
+﻿using DAL.Repository.Abstract;
+using DATA.Model_Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repository.Concrete
+{
+    public class ArticleConcrete
+    {
+        public IRepository<Article> _articleRepository;
+        public IUnitOfWork _articleUnitOfWork;
+        private Context _dbContext;
+
+        public ArticleConcrete()
+        {
+            _dbContext = new Context();
+            _articleUnitOfWork = new EFUnitOfWork(_dbContext);
+            _articleRepository = _articleUnitOfWork.GetRepository<Article>();
+        }
+    }
+}
